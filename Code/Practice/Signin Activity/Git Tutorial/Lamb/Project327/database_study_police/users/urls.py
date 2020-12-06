359036cfd8c807bd4d6f58909b5a08ed2a30d91c
Farhat Lamisa Rahman 1731068 <lamisa.farhat@northsouth.edu>
2020-12-06 13:28:01 +0600

    Issue #6: Login Activity partially done

---
+++ Code/Practice/Signin Activity/Git Tutorial/Lamb/Project327/database_study_police/users/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('exists/<email>', views.AccountExistsAPICall.as_view(), name = 'exists'),
+    path('registration/', views.CustomRegisterView.as_view(), name = "customRegister"),
+    path('google/callback/', views.GoogleCallback.as_view(), name = "googleRedirect"),
+    path('google/login/', views.GoogleLoginFromToken.as_view(), name = "googleLogin"),
+    path('provider/taken/', views.ProviderTakenEmail.as_view(), name = "emailTakenProvider"),
+]
