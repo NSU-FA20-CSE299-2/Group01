df2a40c23465b51b2fac5be54222a6cf8bbc720a
Farhat Lamisa Rahman 1731068 <lamisa.farhat@northsouth.edu>
2020-12-06 23:53:32 +0600

    Issue #6: Sign-in Activity modified

--- Code/Practice/SignIn_Activity/app/src/main/java/com/example/signin_activity/LogInWithPasswordActivity.java
+++ Code/Practice/SignIn_Activity/app/src/main/java/com/example/signin_activity/LogInWithPasswordActivity.java
@@ -1,20 +1,24 @@
 package com.example.signin_activity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+public class LogInWithPasswordActivity extends AppCompatActivity implements View.OnClickListener {
 
-
-public class MainActivity extends AppCompatActivity  {
     TextView LogInWithPasswordActivity;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_log_in_with_password);
+
+    }
+
+    @Override
+    public void onClick(View v) {
 
     }
 }
--- Code/Practice/SignIn_Activity/app/src/main/java/com/example/signin_activity/SignInActivity.java
+++ Code/Practice/SignIn_Activity/app/src/main/java/com/example/signin_activity/SignInActivity.java
@@ -1,20 +1,14 @@
 package com.example.signin_activity;
 
-import android.os.Bundle;
-import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
 
-
-public class MainActivity extends AppCompatActivity  {
-    TextView LogInWithPasswordActivity;
-
+public class SignInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_sign_in);
     }
 }
