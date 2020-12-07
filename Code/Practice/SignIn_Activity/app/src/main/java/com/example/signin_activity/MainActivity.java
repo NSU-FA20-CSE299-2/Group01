802df5f5f789f2d013302d1bb2b031c5f7318268
Farhat Lamisa Rahman 1731068 <lamisa.farhat@northsouth.edu>
2020-12-07 13:58:58 +0600

    Issue #6: UI for login activity completed

--- Code/Practice/SignIn_Activity/app/src/main/java/com/example/signin_activity/MainActivity.java
+++ Code/Practice/SignIn_Activity/app/src/main/java/com/example/signin_activity/MainActivity.java
@@ -1,20 +1,36 @@
 package com.example.signin_activity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class MainActivity extends AppCompatActivity  {
-    TextView LogInWithPasswordActivity;
-
+  EditText medit_Email;
+  Button mproceed_button;
+  private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        medit_Email = findViewById(R.id.edit_email);
+        mproceed_button= findViewById(R.id.proceed_button);
+        mAuth = FirebaseAuth.getInstance();
+
+        mproceed_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),LogInWithPasswordActivity.class));
+            }
+        });
+
 
     }
 }
