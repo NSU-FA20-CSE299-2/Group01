ece12cc2411fa63a720355e9976d8bcb9531ff02
Khan Afnan Rahad <afnan.rahat@northsouth.edu>
2020-12-14 16:32:02 +0600

    Issue #11 : Firebase Testing
    Authentication for signup and sign updated.

--- Code/Practice/FirebaseTest/app/src/main/java/com/example/firebasetest/SignUp.java
+++ Code/Practice/FirebaseTest/app/src/main/java/com/example/firebasetest/SignUp.java
@@ -1,14 +1,91 @@
 package com.example.firebasetest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUp extends AppCompatActivity {
+    EditText signUpNameEditText, signUpEmailEditText, signUpPasswordEditText;
+    Button signUpButton;
+    TextView signInTextView;
+    ProgressBar progressBar;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        signUpNameEditText = findViewById(R.id.signUpNameEditTextId);
+        signUpEmailEditText = findViewById(R.id.signUpEmailEditTextId);
+        signUpPasswordEditText = findViewById(R.id.signUpPasswordEditTextId);
+        signUpButton = findViewById(R.id.signUpButtonId);
+        signInTextView = findViewById(R.id.signInTextViewId);
+
+        mAuth = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.progressBarId);
+
+        if (mAuth.getCurrentUser() != null) {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = signUpEmailEditText.getText().toString().trim();
+                String password = signUpPasswordEditText.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    signUpEmailEditText.setError(("Email is required"));
+                    return;
+                }
+
+                if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+                {
+                    signUpEmailEditText.setError("Enter a valid email address");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    signUpPasswordEditText.setError("Password is required");
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    signUpPasswordEditText.setError("Minimum six in length is required");
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(SignUp.this, "Registered Successfully",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        } else {
+                            Toast.makeText(SignUp.this, "Error!" + task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
     }
 }
