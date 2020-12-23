4143d4dbee406d997332cccb770429d4d52b49c7
Farhat Lamisa Rahman 1731068 <lamisa.farhat@northsouth.edu>
2020-12-23 23:25:36 +0600

    Enhancement #14: Connected Sign_In_Activity with Log_In_With_Password_Activity

--- Code/StudyTracker/app/src/main/java/com/example/studytracker/SignInActivity.java
+++ Code/StudyTracker/app/src/main/java/com/example/studytracker/SignInActivity.java
@@ -2,13 +2,31 @@ package com.example.studytracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignInActivity extends AppCompatActivity {
+    EditText medit_Email;
+    Button mproceed_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign__in_);
+        medit_Email = findViewById(R.id.edit_email);
+        mproceed_button = findViewById(R.id.proceed_button);
+
+        mproceed_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(getApplicationContext(),LogInWithPasswordActivity.class));
+
+            }
+        });
+
     }
 }
