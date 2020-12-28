0cbffbfd889d83729b4a6aca321dbae21bde5bb2
Khan Afnan Rahad <afnan.rahat@northsouth.edu>
2020-12-28 12:26:48 +0600

    Enhancement #14 : Authentication updated
    Firebase authentication updated for the Signin activity.

--- Code/StudyTracker/app/src/main/java/com/example/studytracker/LogInWithPasswordActivity.java
+++ Code/StudyTracker/app/src/main/java/com/example/studytracker/LogInWithPasswordActivity.java
@@ -3,12 +3,19 @@ package com.example.studytracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LogInWithPasswordActivity extends AppCompatActivity {
+
+    TextView label;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in_with_password);
+
+        label = findViewById(R.id.logInEmailAddress);
+
+        tv_email
     }
 }
--- Code/StudyTracker/app/src/main/java/com/example/studytracker/SignInActivity.java
+++ Code/StudyTracker/app/src/main/java/com/example/studytracker/SignInActivity.java
@@ -1,29 +1,78 @@
 package com.example.studytracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class SignInActivity extends AppCompatActivity {
-    EditText medit_Email;
-    Button mproceed_button;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignInActivity extends AppCompatActivity{
+    EditText st_email, st_password;
+    Button button;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign__in_);
-        medit_Email = findViewById(R.id.edit_email);
-        mproceed_button = findViewById(R.id.proceed_button);
+        st_email = findViewById(R.id.logInEmailAddress);
+        st_password = findViewById(R.id.logInPassword);
+        button = findViewById(R.id.proceed_button);
 
-        mproceed_button.setOnClickListener(new View.OnClickListener() {
+        mAuth = FirebaseAuth.getInstance();
+
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 startActivity(new Intent(getApplicationContext(),LogInWithPasswordActivity.class));
+
+                String email = st_email.getText().toString().trim();
+                String password = st_password.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    st_email.setError(("Email is required"));
+                    return;
+                }
+
+                if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+                {
+                    st_email.setError("Enter a valid email address");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    st_password.setError("Password is required");
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    st_password.setError("Minimum six in length is required");
+                    return;
+                }
+
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(SignInActivity.this, "Logged in Successfully",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        } else {
+                            Toast.makeText(SignInActivity.this, "Error!" + task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
             }
         });
