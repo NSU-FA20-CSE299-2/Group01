427545651f2cfcd303425a0b4fd76c73b6dd9306
Farhat Lamisa Rahman 1731068 <lamisa.farhat@northsouth.edu>
2021-01-06 21:57:01 +0600

    Enahancement #16: Created dialog to join class

--- Code/StudyTracker/app/src/main/java/com/example/studytracker/MainActivity.java
+++ Code/StudyTracker/app/src/main/java/com/example/studytracker/MainActivity.java
@@ -1,29 +1,86 @@
 package com.example.studytracker;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView mcreate_class_option;
+    private FloatingActionButton fabCreateJoinClass;
+    private Dialog mDialog;
+    private TextView tvDiaJoinClass;
+    private TextView tvbDiaJoinClass;
+    private TextView tvDiaCreateClass;
+    private TextView tvbDiaCreateClass;
+    private TextView mtvUserName;
+    private ImageView mivUserPic;
+    private EditText etvAccessCode;
+    private EditText etvClassName;
+    private TextView mtvbLogOut;
+    private ConstraintLayout diaCreateJoinCont;
+    private ConstraintLayout diaJoinCont;
+    private ConstraintLayout diaCreateCont;
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        /*requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getSupportActionBar().hide();
+        getSupportActionBar().hide();*/
         setContentView(R.layout.activity_main);
 
-        startActivity(new Intent(getApplicationContext(), SignUpActivity.class));
+        addToolBar();
+        setDialog();
+
+        //startActivity(new Intent(getApplicationContext(), SignUpActivity.class));
+
+        fabCreateJoinClass = findViewById(R.id.fab_create_join_class);
+        
+
+    }
+
+    private void setDialog() {
+        mDialog = new Dialog(this);
+        mDialog.setContentView(R.layout.dialog_create_join_class);
+        tvDiaJoinClass = mDialog.findViewById(R.id.join_class_option);
+        tvbDiaJoinClass = mDialog.findViewById(R.id.join_class_button);
+        tvDiaCreateClass = mDialog.findViewById(R.id.create_class_option);
+        tvbDiaCreateClass = mDialog.findViewById(R.id.create_class_button);
+        diaCreateJoinCont = mDialog.findViewById(R.id.create_join_container);
+        diaJoinCont = mDialog.findViewById(R.id.join_container);
+        diaCreateCont = mDialog.findViewById(R.id.create_container);
+        etvAccessCode = mDialog.findViewById(R.id.edit_join_class);
+        etvClassName = mDialog.findViewById(R.id.edit_create_class);
+        tvDiaJoinClass.setOnClickListener(this);
+        tvbDiaJoinClass.setOnClickListener(this);
+        tvDiaCreateClass.setOnClickListener(this);
+        tvbDiaCreateClass.setOnClickListener(this);
+    }
+
+    private void addToolBar() {
+        Toolbar toolbar = findViewById(R.id.sp_main_toolbar);
+        toolbar.showOverflowMenu();
+        setSupportActionBar(toolbar);
     }
 
     @Override
     public void onClick(View view) {
+        openDialog();
 
     }
+
+    private void openDialog() {
+    }
+
+
 }
